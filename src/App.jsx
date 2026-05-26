e832799d1545b4f1220dcf217bd02e3c864de524
Icha Marisa Mahmuda <chaloeymae26@gmail.com>
2026-05-26 12:03:33 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-6xl font-extrabold text-purple-600 tracking-tight">
+        Architectify
+      </h1>
+    </div>
+  )
+}
+
+export default App
